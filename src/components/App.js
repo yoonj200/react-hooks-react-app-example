ad684b005dedc95d4b14fa88d59956e51f11e8a4
Joshua <yoonj200@gmail.com>
2022-03-02 12:19:37 -0600

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
-import React from "react";
-import { format } from "date-fns";
+import React from "react"; // 'react' and 'date-fns' are npm packages
+import { format } from "date-fns"; 
+// App.js pulls packages' specific content by importing from 'node_modules' folder 
+// (created when we ran npm install)
 import ExampleComponent from "./ExampleComponent";
+import TestComponent from "./TestComponent"
 
 // Add your code own within the return statement
 function App() {
-  return (
+  return ( // returns JSX (JS XML) code, which is transpiled to JS code
     <div className="App">
-      <h1>{format(new Date(), "MMMM do yyyy, h:mm:ss a")}</h1>
+      {/* <h1>{format(new Date(), "MMMM do yyyy, h:mm:ss a")}</h1> */}
+      <h1>Now</h1>
       <p className="App-intro">
         In React apps, we write JSX - it looks like HTML, and uses a lot of HTML
         syntax. JSX lets us include JavaScript functions right along with the
@@ -14,8 +18,9 @@ function App() {
         self-contained chunks of JSX.
       </p>
       <ExampleComponent />
+      <TestComponent />
     </div>
-  );
+  ); 
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,3 +4,6 @@ import App from "./components/App";
 import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+// render takes in 2 arguments
+// React component to render (typically top-level App component)
+// Dom element where we want RC to be rendered (usually div with ID of root)
